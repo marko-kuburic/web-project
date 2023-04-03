abe586f157ab618ed4ceeb8c0ba393ce701ce372
Danilo Damjanovic <danilodanilod@gmail.com>
2023-04-03 14:25:04 +0200

    Initial commit

---
+++ src/test/java/acs/uns/ac/rs/webproject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package acs.uns.ac.rs.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

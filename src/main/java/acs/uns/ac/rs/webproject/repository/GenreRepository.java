56bd164e1e805771f19e1dd22d3959130db6d849
Danilo Damjanovic <danilodanilod@gmail.com>
2023-04-07 21:22:36 +0200

    Deleted genre repository

--- src/main/java/acs/uns/ac/rs/webproject/repository/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package acs.uns.ac.rs.webproject.repository;
-
-import acs.uns.ac.rs.webproject.entity.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface GenreRepository extends JpaRepository <Genre, Long> {
-}

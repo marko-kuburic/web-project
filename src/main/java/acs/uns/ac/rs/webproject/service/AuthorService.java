fad7ca5c01ecc96c585506ab0b66a5b479976d3a
Danilo Damjanovic <danilodanilod@gmail.com>
2023-04-09 14:11:53 +0200

    Adding Author service

--- src/main/java/acs/uns/ac/rs/webproject/service/AuthorService.java
+++ src/main/java/acs/uns/ac/rs/webproject/service/AuthorService.java
@@ -1,4 +1,35 @@
 package acs.uns.ac.rs.webproject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import acs.uns.ac.rs.webproject.entity.Author;
+import acs.uns.ac.rs.webproject.repository.AuthorRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public Author findOne(Long id)
+    {
+        Optional<Author> foundAuthor = authorRepository.findById(id);
+        if (foundAuthor.isPresent())
+            return foundAuthor.get();
+
+        return null;
+    }
+
+    public List<Author> findAllByName(String name){return authorRepository.findAllByName(name);}
+
+    public List<Author> findAllBySurname(String surname){return authorRepository.findAllBySurname(surname);}
+    public List<Author> findAllByIsActive(Boolean active){return authorRepository.findAllByIsActive(active);}
+
+    public List<Author> findAll(){ return authorRepository.findAll();}
+
+    public Author save(Author author){ return authorRepository.save(author);}
+
 }
